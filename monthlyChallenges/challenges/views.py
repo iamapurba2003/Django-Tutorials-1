88b7961fad5fc3cc1601a1ff77c8c372dc12bd88
himangshuishere <dehimangshu2020@outlook.com>
2021-12-15 12:21:12 +0530

    ReAdded project monthlyChallenge

---
+++ monthlyChallenges/challenges/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def january(request):
+    return HttpResponse("This works")
+
+
+def february(request):
+    return HttpResponse("This work for february")
