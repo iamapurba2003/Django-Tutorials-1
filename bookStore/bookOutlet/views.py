45527ff7cd73361a9d39963668e04694246346ea
himangshuishere <dehimangshu2020@outlook.com>
2021-12-19 00:10:45 +0530

    Added a new project bookStore and created apps using Django

---
+++ bookStore/bookOutlet/views.py
@@ -0,0 +1,29 @@
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, render
+from .models import Book
+
+# Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, "bookOutlet/index.html", {
+        "books":books
+    })
+
+
+def bookDetail(request, id):
+    # try:
+    #     books = Book.objects.get(pk=id)
+    # except Exception:
+    #     raise Http404
+    
+    # the below code also execute the same way as the above code block...
+    
+    books = get_object_or_404(Book, pk=id)
+    
+    return render(request, "bookOutlet/bookDetail.html",{
+        "bookTitle":books.title,
+        "bookAuthor": books.author,
+        "bookRating":books.rating,
+        "bookBestSelling":books.isBestSelling
+    })
