51eb2ff64ca28fe54cc5d384db9244798c1e336f
himangshuishere <dehimangshu2020@outlook.com>
2021-12-22 11:37:36 +0530

    Deleted TTFBlog Project

--- TTFBlog/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("posts/", views.posts, name="post"),
-    path('posts/<int:id>', views.postDetail, name="post-detail")
-]
